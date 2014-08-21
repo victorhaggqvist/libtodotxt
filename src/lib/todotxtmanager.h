ab3e8b02be29e115085dfb4b058d81b0544d8aa5
Victor Häggqvist <victor@snilius.com>
2014-08-21 19:26:09 +0200

    interface

--- src/lib/todotxtmanager.h
+++ src/lib/todotxtmanager.h
@@ -1,7 +1,7 @@
 #ifndef TODOTXTMANAGER_H
 #define TODOTXTMANAGER_H
 
-#include <vector>
+#include <deque>
 #include <todoitem.h>
 
 namespace Snilius {
@@ -9,11 +9,12 @@ namespace Snilius {
 class TodotxtManager {
   public:
     virtual ~TodotxtManager() {}
-    virtual std::vector<TodoItem> getTodoList() = 0;
+    virtual std::deque<TodoItem> getTodoList() = 0;
     virtual void updateItem(int index, TodoItem& item) = 0;
     virtual void newItem(TodoItem& item) = 0;
     virtual void removeItem(int index) = 0;
     virtual void removeItem(TodoItem& item) = 0;
+    virtual void archiveDoneItems() = 0;
     virtual void archiveItem(int index) = 0;
     virtual void archiveItem(TodoItem& item) = 0;
 };
