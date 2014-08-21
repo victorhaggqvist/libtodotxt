5ab4274765fb805092b35093414b91a2bf5df3f2
Victor Häggqvist <victor@snilius.com>
2014-08-21 19:24:05 +0200

    wrap it in git

---
+++ src/lib/todotxtgit.cpp
@@ -0,0 +1,5 @@
+#include "todotxtgit.h"
+
+Todotxtgit::Todotxtgit()
+{
+}
---
+++ src/lib/todotxtgit.h
@@ -0,0 +1,10 @@
+#ifndef TODOTXTGIT_H
+#define TODOTXTGIT_H
+
+class Todotxtgit
+{
+  public:
+    Todotxtgit();
+};
+
+#endif // TODOTXTGIT_H
