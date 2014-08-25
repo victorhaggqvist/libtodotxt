8f30181bcda4cbc42671a83c3cd5509982de7fd1
Victor Häggqvist <victor@snilius.com>
2014-08-25 04:08:29 +0200

    usemod

--- buildsetup.sh
+++ buildsetup.sh
@@ -1,10 +1,10 @@
-echo "pre stuffs"
-rm -rf libgit2
-git clone https://github.com/libgit2/libgit2.git
-cd libgit2
-git checkout v0.21.1
-cd ..
-
+#echo "pre stuffs"
+#rm -rf libgit2
+#git clone https://github.com/libgit2/libgit2.git
+#cd libgit2
+#git checkout v0.21.1
+#cd ..
+git submodule update --init
 cd test
 rm -rf gtest-1.7.0 gtest-1.7.0.zip gtest
 wget --quiet -Ogtest.zip https://googletest.googlecode.com/files/gtest-1.7.0.zip
