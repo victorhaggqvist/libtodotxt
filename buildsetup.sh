807fd44b11c9093fa2a6a879f3b5a2c254c4dbd5
Victor Häggqvist <victor@snilius.com>
2014-08-25 04:00:26 +0200

    fuckthis

--- buildsetup.sh
+++ buildsetup.sh
@@ -1,26 +1,12 @@
 echo "pre stuffs"
+rm -rf libgit2
+git clone https://github.com/libgit2/libgit2.git
+cd libgit2
+git checkout v0.21.1
+cd ..
 
-echo "checking for libgit2"
-$WAT="$(pwd)/libgit2"
-if [ ! -d "$WAT" ]; then
-  echo "getting libgit2"
-  git clone https://github.com/libgit2/libgit2.git
-  cd libgit2
-  git checkout v0.21.1
-  cd ..
-else
-  echo "libgit2 exists"
-fi
-
-echo "checking for gtest"
-if [ ! -d "./test/gtest" ]; then
-  cd test
-  echo "getting gtest"
-  wget --quiet -Ogtest.zip https://googletest.googlecode.com/files/gtest-1.7.0.zip
-  #rm -rf gtest-1.7.0
-  unzip -qq gtest.zip
-  mkdir gtest
-  mv gtest-1.7.0/* gtest
-else
-  echo "gtest exists"
-fi
+cd test
+rm -rf gtest-1.7.0 gtest-1.7.0.zip gtest
+wget --quiet -Ogtest.zip https://googletest.googlecode.com/files/gtest-1.7.0.zip
+unzip -qq gtest.zip
+mv gtest-1.7.0/* gtest
