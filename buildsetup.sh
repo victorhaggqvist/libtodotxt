ebcb8ea7f4498ce57cd31b427535f9ee9bca37fe
Victor Häggqvist <victor@snilius.com>
2014-08-25 03:31:35 +0200

    xz

--- buildsetup.sh
+++ buildsetup.sh
@@ -1,26 +1,25 @@
 echo "pre stuffs"
-$LIBGIT2DIR="./libgit2"
-$GTESTDIR="./test/gtest"
+
 echo "checking for libgit2"
-if [ ! -d "$LIBGIT2DIR" ]; then
-echo "getting libgit2"
-git clone https://github.com/libgit2/libgit2.git
-cd libgit2
-git checkout v0.21.1
-cd ..
+if [ ! -d "./libgit2" ]; then
+  echo "getting libgit2"
+  git clone https://github.com/libgit2/libgit2.git
+  cd libgit2
+  git checkout v0.21.1
+  cd ..
 else
-echo "libgit2 exists"
+  echo "libgit2 exists"
 fi
 
 echo "checking for gtest"
-if [ ! -d "$GTEST2DIR" ]; then
-cd test
-echo "getting gtest"
-wget --quiet -Ogtest.zip https://googletest.googlecode.com/files/gtest-1.7.0.zip
-rm -rf gtest-1.7.0
-unzip -qq gtest.zip
-mkdir gtest
-mv gtest-1.7.0/* gtest
+if [ ! -d "./test/gtest" ]; then
+  cd test
+  echo "getting gtest"
+  wget --quiet -Ogtest.zip https://googletest.googlecode.com/files/gtest-1.7.0.zip
+  #rm -rf gtest-1.7.0
+  unzip -qq gtest.zip
+  mkdir gtest
+  mv gtest-1.7.0/* gtest
 else
-echo "gtest exists"
+  echo "gtest exists"
 fi
